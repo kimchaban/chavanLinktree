5e8bbe53a7e71b29cfe5f9ff2777b101ad70341f
soheekim.dev <soheekim.dev@gmail.com>
2023-06-15 22:53:58 +0900

    전송 완료 메시지가 푸터에 가려지는 현상 해결

--- script.js
+++ script.js
@@ -15,3 +15,8 @@ let option = {
 };
 
 observer.observe(thxMsg, option);
+
+// 전송 완료 메시지 bottom 값 설정
+const footer = document.querySelector('footer');
+let footerHeight = footer.offsetHeight;
+thxMsg.style.bottom = `${footerHeight}px`;
